19586d716d00bf3c565e2af5cd5c9f7b595dfd98
hyunmin <choihm9903@naver.com>
2022-05-11 14:09:39 +0900

    feat: naver-map

--- app/src/main/java/com/example/sumubi/MainActivity.kt
+++ app/src/main/java/com/example/sumubi/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.example.sumubi
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -10,6 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
         home.setOnClickListener { startActivity(Intent(this, MainActivity::class.java)) }
         planner.setOnClickListener { startActivity(Intent(this, PlannerActivity::class.java)) }
